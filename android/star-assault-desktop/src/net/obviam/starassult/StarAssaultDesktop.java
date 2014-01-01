d61877b3e788628fe501d2b9523ff4a3d6b5d268
Keith Mikoleit <keith.mikoleit@gmail.com>
2013-12-31 16:11:28 -0800

    Added StarAssaultActivity and StarAssaultDesktop.  These are the andoird and desktop launchers.

---
+++ android/star-assault-desktop/src/net/obviam/starassult/StarAssaultDesktop.java
@@ -0,0 +1,9 @@
+package net.obviam.starassult;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+
+public class StarAssaultDesktop {
+	public static void main(String[] args) {
+		new LwjglApplication(new StarAssault(), "Star Assault", 480, 320, true);
+	}
+}
